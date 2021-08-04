a55f01623d836ac0a3bcfbe48e3edfaf326e9220
TrsckySc <xieshaka5995@163.com>
2021-08-04 17:37:14 +0800

    完善可视化页面的增删改查功能

--- controllers/index.js
+++ controllers/index.js
@@ -17,8 +17,8 @@ db.once('open', function () {
 });
 
 // 客户端页面
-router.get('/',(req,res)=>{
-    var page = fs.readFileSync('./index.html',{encoding:"utf8"});
+router.get('/', (req, res) => {
+    var page = fs.readFileSync('./index.html', { encoding: "utf8" });
     res.send(page);
 })
 
@@ -56,13 +56,78 @@ router.post('/api/add-interface', (req, res) => {
 
 // 获取接口列表
 router.get('/api/get-interface-list', (req, res) => {
-    
-    TodoModal.find(req.query,(err, items) => {
+    var param = {};
+    // 添加接口名称入参
+    if (req.query && req.query.name) {
+        param.name = { $regex: req.query.name }
+    }
+    // 添加接口地址入参
+    if (req.query && req.query.url) {
+        param.url = { $regex: req.query.url }
+    }
+
+    TodoModal.find(param, (err, items) => {
         if (err) throw err;
         res.send({
             success: true,
             data: items
         })
+    })
+})
+
+// 删除接口
+router.post('/api/delete-interface', (req, res) => {
+    if (!req.body || !req.body.id) {
+        res.send({
+            success: false,
+            errorMsg: "缺少接口id"
+        })
+    }
+    TodoModal.deleteOne({ _id: req.body.id }, (err) => {
+        if (err) throw err;
+        res.send({
+            success: true
+        })
+    })
+})
+
+// 获取单个接口详情
+router.post('/api/get-interface-detail', (req, res) => {
+    if (!req.body || !req.body.id) {
+        res.send({
+            success: false,
+            errorMsg: "缺少接口id"
+        })
+    }
+    TodoModal.findById(req.body.id, (err, item) => {
+        if (err) throw err;
+        res.send({
+            success: true,
+            data: item
+        })
+    })
+})
+
+// 更新单个接口
+router.post('/api/update-interface', (req, res) => {
+    if (!req.body || !req.body.id) {
+        res.send({
+            success: false,
+            errorMsg: "缺少接口id"
+        })
+    }
+    TodoModal.findById(req.body.id, (err, item) => {
+        if (err) throw err;
+        item.name = req.body.name;
+        item.url = req.body.url;
+        item.data = req.body.data;
+        item.save((err) => {
+            if (err) throw err;
+            res.send({
+                success: true
+            })
+        })
+
     })
 })
 
