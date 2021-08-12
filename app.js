b310a2a3274769408d53ce9da54a2a5ef22b3743
TrsckySc <xieshaka5995@163.com>
2021-08-12 20:28:04 +0800

    修改对于map文件的无响应请求

--- app.js
+++ app.js
@@ -13,14 +13,14 @@ app.set('view engine', 'ejs');
 
 app.use(express.static('./client'));
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(router);
 
 app.use((req, res, next) => {
   // 拿到地址，去数据库中查询，如果没有则用axios调用接口，否则返回数据库里面的mock数据字段
-  TodoModal.find({url: req.path}, (err, item) => {
+  TodoModal.find({ url: req.path }, (err, item) => {
     if (err) throw err;
     // 查询到数据并且开启了mock状态，则返回mock数据
     if (JSON.stringify(item) !== "[]" && item[0].data.isOpen) {
@@ -32,7 +32,16 @@ app.use((req, res, next) => {
     }
 
     // 否则做接口转发
-    if (req.method == "GET") {
+    if (req.url.indexOf('/lib/') >= 0 || req.url.indexOf('/favicon') >= 0) {
+      // 针对map文件与favicon文件做了下错误终止
+      axios.get(req.url)
+        .then(function (response) {
+          res.send(response);
+        })
+        .catch(function (error) {
+          res.status(404).end();
+        });
+    } else if (req.method == "GET") {
       const resp = axios({
         method: "get",
         url: "http://12.168.3.34:8001" + req.url,
--- controllers/index.js
+++ controllers/index.js
@@ -9,7 +9,7 @@ var ejs = require('ejs');
 
 var TodoModal = require('./modal');
 
-mongoose.connect('mongodb://localhost/myapp', {useNewUrlParser: true});
+mongoose.connect('mongodb://localhost/myapp', { useNewUrlParser: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
@@ -18,14 +18,14 @@ db.once('open', function () {
 
 // 客户端页面
 router.get('/', (req, res) => {
-  var page = fs.readFileSync('./index.html', {encoding: "utf8"});
+  var page = fs.readFileSync('./index.html', { encoding: "utf8" });
   res.send(page);
 });
 
 // 保存接口
 router.post('/api/add-interface', (req, res) => {
   // 保证url唯一性
-  TodoModal.find({url: req.body.url}, (err, item) => {
+  TodoModal.find({ url: req.body.url }, (err, item) => {
     if (err) throw err;
     if (JSON.stringify(item) !== "[]") {
       res.send({
@@ -53,11 +53,11 @@ router.get('/api/get-interface-list', (req, res) => {
   var param = {};
   // 添加接口名称入参
   if (req.query && req.query.name) {
-    param.name = {$regex: req.query.name}
+    param.name = { $regex: req.query.name }
   }
   // 添加接口地址入参
   if (req.query && req.query.url) {
-    param.url = {$regex: req.query.url}
+    param.url = { $regex: req.query.url }
   }
 
   TodoModal.find(param, (err, items) => {
@@ -77,7 +77,7 @@ router.post('/api/delete-interface', (req, res) => {
       errorMsg: "缺少接口id"
     })
   }
-  TodoModal.deleteOne({_id: req.body.id}, (err) => {
+  TodoModal.deleteOne({ _id: req.body.id }, (err) => {
     if (err) throw err;
     res.send({
       success: true
