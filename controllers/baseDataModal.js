6690e975ff6152e9de2a87cd4fa87f81c1ec891c
TrsckySc <xieshaka5995@163.com>
2021-11-01 05:55:55 +0800

    更改栏目名称修复无法保存空对象的问题

--- controllers/baseDataModal.js
+++ controllers/baseDataModal.js
@@ -5,7 +5,7 @@ var baseDataSchemas = mongoose.Schema({
   data: Object,                       // 基础数据
   aceData: String,                    // ace源数据
   type: String,                       // 基础数据类型
-});
+}, { minimize: false });
 
 var BaseDataModal = mongoose.model('baseData', baseDataSchemas);
 
